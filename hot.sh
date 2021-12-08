cfd2dbababccc6c125e32a7c712e543be8301a55
Neil <nlf11@hotmail.com>
2021-12-08 17:12:11 -0600

    neil

---
+++ hot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Description
+# Author
+# Date 
+
+echo "what is your name"
+read NAME
+
+lscpu
+if
+[ $? -eq 0 ]
+then
+echo "command successful, ${NAME}
+else
+echo "command unsuccessful, ${NAME}
+
+cat mate
+
+[ $? -eq 0 ]
+then
+echo "command successful, ${NAME}
+else 
+echo "command unsuccessful, ${NAME}
+fi
